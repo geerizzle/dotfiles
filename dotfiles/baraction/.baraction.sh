59a8c1d83fe95b31b66397b4022bd589e1702b12
geerizzle <dekidudukovic@gmail.com>
2020-04-26 03:50:08 +0200

    Renamed .baraction.sh

--- dotfiles/baraction/.baraction.sh
+++ dotfiles/baraction/.baraction.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-# baraction.sh for spectrwm status bar
 
-## DISK
 hdd() {
   hdd="$(df -h | awk 'NR==4{print $3, $5}')"
   echo -e "HDD: $hdd"
 }
 
-## RAM
 mem() {
   mem=`free | awk '/Mem/ {printf "%dM", $3 / 1024.0, $2 / 1024.0 }'`
-  echo -e "MEM: $mem"
+  echo -e "RAM: $mem"
 }
 
-## CPU
 cpu() {
   read cpu a b c previdle rest < /proc/stat
   prevtotal=$((a+b+c+previdle))
@@ -24,20 +20,14 @@ cpu() {
   echo -e "CPU: $cpu%"
 }
 
-## VOLUME
 vol() {
     vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
-    echo -e "VOL: $vol"
+    echo -e "VOL: $vol "
 }
 
 SLEEP_SEC=2
-#loops forever outputting a line every SLEEP_SEC secs
 
-# It seems that we are limited to how many characters can be displayed via
-# the baraction script output. And the the markup tags count in that limit.
-# So I would love to add more functions to this script but it makes the 
-# echo output too long to display correctly.
 while :; do
-    echo "$(cpu) | $(mem) | $(hdd) $(vol)" 
+    echo "$(cpu) |  $(mem) | $(hdd) | $(vol)" 
     sleep $SLEEP_SEC
 done
